872f2c13bb4463aa20249fb921a4bf52764d0007
Debbie Vulcan <debbievulcan@Debbies-Air.hsd1.co.comcast.net>
2018-08-19 14:03:56 -0600

    Using Theory

--- myTests/MathHelper_maxshould.cs
+++ myTests/MathHelper_maxshould.cs
@@ -6,12 +6,24 @@ namespace myTests
 {
     public class MathHelper_MaxShould
     {
-        [Fact]
-        public void ReturnGivenValue()
+        private readonly MathHelper _mathHelper;
+
+        public MathHelper_MaxShould()
         {
-             var  mathHelper = new MathHelper();
-             var result = mathHelper.Max(0,0);
-             Assert.Equal(0,result);
+            _mathHelper= new MathHelper();
+        }
+        
+        [Theory]
+        [InlineDataAttribute(89,89,89)]
+        [InlineDataAttribute(0,0,0)]
+        [InlineDataAttribute(2,2,2)]
+        [InlineDataAttribute(1,1,1)]
+
+        public void ReturnGivenValue(int value1, int value2, int expectedvalue)
+        {
+             
+             var result = _mathHelper.Max(value1,value2);
+             Assert.Equal(expectedvalue,result);
              //if(result == 0)
              //{
              //    Console.WriteLine("Sucess"); 
